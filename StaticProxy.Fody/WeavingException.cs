3003926a9f9b7678ec2f4198bec8ea0ec57b86d4
Bruno Juchli <bruno.juchli@innocreative.ch>
2014-04-29 12:49:23 +0200

    updated FluentAssertions

--- StaticProxy.Fody/WeavingException.cs
+++ StaticProxy.Fody/WeavingException.cs
@@ -1,9 +1,16 @@
 using System;
+using System.Runtime.Serialization;
 
+[Serializable]
 public class WeavingException : Exception
 {
     public WeavingException(string message)
         : base(message)
+    {
+    }
+
+    protected WeavingException(SerializationInfo info, StreamingContext context)
+        : base(info, context)
     {
     }
 }
