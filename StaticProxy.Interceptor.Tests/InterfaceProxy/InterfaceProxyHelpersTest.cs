feea7d698c82f1aebc93bbe7c0ca84077bc8aed1
Bruno Juchli <bruno.juchli@innocreative.ch>
2014-05-04 17:17:13 +0200

    moving code which is required by all IoC StaticProxy extensions to this library.

---
+++ StaticProxy.Interceptor.Tests/InterfaceProxy/InterfaceProxyHelpersTest.cs
@@ -0,0 +1,45 @@
+﻿namespace StaticProxy.Interceptor.Tests.InterfaceProxy
+{
+    using System;
+
+    using FluentAssertions;
+
+    using StaticProxy.Interceptor.InterfaceProxy;
+
+    using Xunit;
+    using Xunit.Extensions;
+
+    public class InterfaceProxyHelpersTest
+    {
+        [Fact]
+        public void GetImplementationTypeOfInterface_MustReturnImplementationType()
+        {
+            InterfaceProxyHelpers.GetImplementationTypeOfInterface(typeof(IWithImplementation))
+                .Should().Be(typeof(IWithImplementationImplementation));
+        }
+
+        [Theory]
+        [InlineData(typeof(object))]
+        [InlineData(typeof(int))]
+        public void GetImplementationTypeOfInterface_WhenTypeIsNotInterface_MustThrow(Type interfaceType)
+        {
+            this.Invoking(x => InterfaceProxyHelpers.GetImplementationTypeOfInterface(interfaceType))
+                .ShouldThrow<ArgumentOutOfRangeException>()
+                .Where(ex => ex.ParamName == "interfaceType");
+        }
+
+        [Fact]
+        public void GetImplementationTypeOfInterface_WhenThereIsNoImplementation_MustThrow()
+        {
+            this.Invoking(x => InterfaceProxyHelpers.GetImplementationTypeOfInterface(typeof(IWithoutImplementation)))
+                .ShouldThrow<InvalidOperationException>()
+                .Where(ex => ex.Message.Contains("There is no auto-generated implementation"));
+        }
+
+        public interface IWithImplementation { }
+
+        public class IWithImplementationImplementation { }
+
+        public interface IWithoutImplementation { }
+    }
+}
---
+++ StaticProxy.Interceptor/InterfaceProxy/InterfaceProxyHelpers.cs
@@ -0,0 +1,52 @@
+﻿namespace StaticProxy.Interceptor.InterfaceProxy
+{
+    using System;
+    using System.Globalization;
+
+    public class InterfaceProxyHelpers
+    {
+        public const string InterfaceImplementationSuffix = "Implementation";
+
+        public static Type GetImplementationTypeOfInterface(Type interfaceType)
+        {
+            EnsureTypeIsInterface(interfaceType);
+
+            return RetrieveProxyType(interfaceType);
+        }
+
+        private static void EnsureTypeIsInterface(Type interfaceType)
+        {
+            if (!interfaceType.IsInterface)
+            {
+                string message = string.Format(
+                    CultureInfo.InvariantCulture,
+@"`{0}` Is not an interface. StaticProxy.Fody creates an implementation for every interface where you put the [StaticProxy] on.
+For class proxies, use the appropriate binding. See your IoC extension documentation.",
+                    interfaceType.FullName);
+                throw new ArgumentOutOfRangeException("interfaceType", message);
+            }
+        }
+
+        private static Type RetrieveProxyType(Type interfaceType)
+        {
+            string proxyTypeName = string.Concat(interfaceType.FullName, InterfaceImplementationSuffix);
+            Type proxyType = interfaceType.Assembly.GetType(proxyTypeName);
+            if (proxyType == null)
+            {
+                string message = string.Format(
+                    CultureInfo.InvariantCulture,
+@"There is no auto-generated implementation for interface `{0}`.
+Verify the following:
+ - Put the attribute [StaticProxy] on the interface '{0}'. The attribute can be found in the nuget package `StaticProxy.Interceptor`.
+ - Add the nuget packages `Fody` and `StaticProxy.Fody` to the assembly containing the interface `{0}`
+As a result there should be a class named `{1}` in the same assembly as the interface",
+                    interfaceType.FullName,
+                    proxyTypeName);
+
+                throw new InvalidOperationException(message);
+            }
+
+            return proxyType;
+        }
+    }
+}
