24b904581716accdd24b7f492b5a467d6188910e
Bruno Juchli <bruno.juchli@innocreative.ch>
2016-04-10 12:57:46 +0200

    using gitlink for StaticProxy.Interceptor

---
+++ StaticProxy.Interceptor.Build.bat
@@ -0,0 +1,33 @@
+SET Configuration=Release
+SET Action=Build
+SET UserTarget=Build
+
+SET ResultDir=.\Results
+
+SET BuildFile=StaticProxy.Interceptor.sln
+SET BuildLogFile=%BuildFile%
+
+SET MSBuildPath=%ProgramFiles(x86)%\MSBuild\14.0\Bin\
+SET MSBuildParameters=/maxcpucount /nodeReuse:false /nologo /detailedsummary /consoleloggerparameters:Summary;Verbosity=minimal
+SET MSBuildFileLogParameters=/fl1 /flp1:Summary;Verbosity=normal;LogFile=%BuildLogFile%.log
+SET MSBuildErrorFileLogParameters=/fl2 /flp2:NoSummary;ErrorsOnly;LogFile=%BuildLogFile%.errors.log
+SET MSBuildWarningFileLogParameters=/fl3 /flp3:NoSummary;WarningsOnly;LogFile=%BuildLogFile%.warnings.log
+
+
+"%MSBuildPath%\MSBUILD.EXE" %BuildFile% /t:%UserTarget% %MSBuildParameters% %MSBuildFileLogParameters% %MSBuildErrorFileLogParameters% %MSBuildWarningFileLogParameters%
+IF ERRORLEVEL 1 GOTO Failed
+
+gitlink.exe . -f %BuildFile% -ignore StaticProxy.Interceptor.Tests
+IF ERRORLEVEL 1 GOTO Failed
+
+CALL %PrintSuccessful%
+GOTO End
+
+:Failed
+CALL %PrintFailed%
+
+:End
+CALL %PrintTime%
+
+rem pause if double clicked only
+if %0 == "%~0" pause
