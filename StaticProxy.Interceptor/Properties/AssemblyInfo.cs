6b2f97de53a094ebb13a28403ce52e3de34e8826
Bruno Juchli <bruno.juchli@innocreative.ch>
2016-04-10 10:36:20 +0200

    re-added all version info to AssemblyInfo so that AppVeyor can patch them.

--- StaticProxy.Interceptor/Properties/AssemblyInfo.cs
+++ StaticProxy.Interceptor/Properties/AssemblyInfo.cs
@@ -11,9 +11,22 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyCompany("Bruno Juchli")]
 [assembly: AssemblyProduct("StaticProxy.Interceptor")]
 [assembly: AssemblyCopyright("Copyright © Bruno Juchli 2016")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
+
+// these values are here to be patched by AppVeyor
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
+[assembly: AssemblyInformationalVersion("0.1.0.0")]
 
 [assembly: InternalsVisibleTo("StaticProxy.Interceptor.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
