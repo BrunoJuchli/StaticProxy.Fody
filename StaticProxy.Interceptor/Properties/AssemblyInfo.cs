96f7351afd8c57e8f9474c56b73036fcc1c5ed3c
Bruno Juchli <bruno.juchli@innocreative.ch>
2016-04-10 10:32:05 +0200

    trying to see if removing assembly version info helps with nuget package building.

--- StaticProxy.Interceptor/Properties/AssemblyInfo.cs
+++ StaticProxy.Interceptor/Properties/AssemblyInfo.cs
@@ -10,23 +10,10 @@ using System.Runtime.CompilerServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Bruno Juchli")]
 [assembly: AssemblyProduct("StaticProxy.Interceptor")]
-[assembly: AssemblyCopyright("Copyright © Bruno Juchli 2014")]
+[assembly: AssemblyCopyright("Copyright © Bruno Juchli 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.0.0")]
-[assembly: AssemblyFileVersion("0.1.0.0")]
 
 [assembly: InternalsVisibleTo("StaticProxy.Interceptor.Tests")]
 [assembly: InternalsVisibleTo("DynamicProxyGenAssembly2")]
