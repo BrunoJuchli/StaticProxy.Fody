1a41458d6565a7ac37c5a1e887e81c75ce2eb7c0
Bruno Juchli <bruno.juchli@innocreative.ch>
2017-02-28 13:06:02 +0700

    added test case for static constructor

---
+++ TestAssemblies/SimpleTest/ClassDecoration/ClassWithStaticConstructor.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+
+namespace SimpleTest.ClassDecoration
+{
+    /// <summary>
+    /// there is no constructor defined and this is a non-static class
+    /// so default constructor (ctor) will be created
+    /// </summary>
+    [StaticProxy]
+    class ClassWithStaticConstructor
+    {
+        /// <summary>
+        /// creates a static constructor (cctor)
+        /// </summary>
+        private static HashSet<int> hashSet = new HashSet<int>()
+        {
+            1,
+            2,
+            3
+        };
+    }
+}
