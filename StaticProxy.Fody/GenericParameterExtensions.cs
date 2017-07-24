9194980975a5577b852bf3f3adc30fc453568052
Bruno Juchli <bruno.juchli@innocreative.ch>
2017-07-24 12:15:29 +0700

    Copying Generic Constraints

--- StaticProxy.Fody/GenericParameterExtensions.cs
+++ StaticProxy.Fody/GenericParameterExtensions.cs
@@ -7,6 +7,11 @@ namespace StaticProxy.Fody
         public static GenericParameter CreateCopy(this GenericParameter original, IGenericParameterProvider owner)
         {
             var copy = new GenericParameter(original.Name, owner);
+
+            foreach(var constraint in original.Constraints) {
+                copy.Constraints.Add(constraint);
+            }
+            
             copy.Attributes = original.Attributes;
             return copy;
         }
